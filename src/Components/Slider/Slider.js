58d4b27668ea76d76c6d6c063aa2f0cd5f734546
Matildad <mtld.desouza@gmail.com>
2021-05-30 14:28:41 +0200

    placing the buttons:sparkles:

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './Slider.css'
 import dataSlider from './dataSlider'
+import BtnSlider from './BtnSlider'
 
 export default function Slider() {
 
@@ -8,6 +9,14 @@ export default function Slider() {
         index: 1,
         inProgress: false
     })
+
+    const nextSlide = () => {
+        console.log("NEXT");
+    }
+
+    const prevSlide = () => {
+        console.log("PREV");
+    }
 
     return (
         <div className="container-slider">
@@ -22,6 +31,8 @@ export default function Slider() {
                     </div>
                 )
             })}
+            <BtnSlider moveSlide={nextSlide} direction= {"next"}/>
+            <BtnSlider moveSlide={prevSlide} direction= {"prev"}/>
         </div>
     )
 }
